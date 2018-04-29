e4cc4f8b8c521e357110005f29823ea3c09916ad
sidisl16 <sidisl16@gmail.com>
2018-04-29 21:32:11 +0530

    implemented dashboard

--- Communication-hub/src/main/java/org/research/iot/communication/hub/model/Reading.java
+++ Communication-hub/src/main/java/org/research/iot/communication/hub/model/Reading.java
@@ -3,10 +3,8 @@ package org.research.iot.communication.hub.model;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldIndex;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Document(indexName = "sensors", type = "readings")
